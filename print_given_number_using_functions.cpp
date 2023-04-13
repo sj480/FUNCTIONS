61dda168006ba81ea4ec21b35118739f92b78685
swapnil shirish janrao <73775658+sj480@users.noreply.github.com>
2023-04-13 16:30:13 +0530

    Add files via upload

---
+++ print_given_number_using_functions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std; 
+
+void display(int a)
+{
+    cout<<a<<endl;
+    
+    return;
+}
+
+int main()
+{
+    int a; 
+    cin>>a;
+
+    cout<<a<<endl;
+
+    return 0; 
+}
